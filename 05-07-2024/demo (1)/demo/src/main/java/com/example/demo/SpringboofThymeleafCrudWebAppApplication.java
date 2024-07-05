d6ecd723412b3c6b1eeda7c1091fca6349ccc808
Anulekshmi2701 <122439413+Anulekshmi2701@users.noreply.github.com>
2024-07-05 17:03:15 +0530

    Add files via upload

---
+++ 05-07-2024/demo (1)/demo/src/main/java/com/example/demo/SpringboofThymeleafCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboofThymeleafCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboofThymeleafCrudWebAppApplication.class, args);
+	}
+
+}
